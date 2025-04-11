94fe835a591df5a7dd09174ffff5722925729d5b
Luiz Carlos <luizzlcs@gmail.com>
2025-04-11 10:54:03 -0300

    classe constants para API

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = 'https://bsky.social/xrpc';
+  static const String loginEndpoint = '/com.atproto.server.createSession';
+  static const String timelineEndpoint = '/app.bsky.feed.getTimeline';
+}
